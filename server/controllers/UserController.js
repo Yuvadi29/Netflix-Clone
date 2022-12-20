376f3b61f693c5312681fff5694147290bffeae0
Yuvadi29 <adityatrivedi612@gmail.com>
2022-12-20 18:16:43 +0530

    Get All Movies

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,16 +1,16 @@
 const User = require("../models/UserModel");
 
-// module.exports.getLikedMovies = async (req, res) => {
-//     try {
-//         const { email } = req.params;
-//         const user = await await User.findOne({ email });
-//         if (user) {
-//             return res.json({ msg: "success", movies: user.likedMovies });
-//         } else return res.json({ msg: "User with given email not found." });
-//     } catch (error) {
-//         return res.json({ msg: "Error fetching movies." });
-//     }
-// };
+module.exports.getLikedMovies = async (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.json({ msg: "Success", movies: user.likedMovies });
+        } else return res.json({ msg: "User with given email not found." });
+    } catch (error) {
+        return res.json({ msg: "Error fetching movies." });
+    }
+};
 
 module.exports.addToLikedMovies = async (req, res) => {
     try {
